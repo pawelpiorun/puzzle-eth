37d2ce30f36af6fde1aa1e2fbc9aca15f96d0c77
Paweł Piorun <dev.pawelpiorun@gmail.com>
2021-10-27 00:11:59 +0200

    first commit

---
+++ contracts/Puzzle.sol
@@ -0,0 +1,44 @@
+// SPDX-License-Identificator: MIT
+
+pragma solidity ^0.8.9;
+
+contract Puzzle {
+    uint public prize;
+    address private manager;
+    bytes32 private answer;
+    
+    event NewAnswer();
+    event AnswerGuessed(address indexed winner, uint prize);
+    
+    constructor() {
+        manager = msg.sender;
+    }
+    
+    modifier onlyManager() {
+        require(msg.sender == manager, "You're not a manager.");
+        _;
+    }
+    
+    function setNewAnswer(string memory word) public payable onlyManager {
+        require(msg.value > 0);
+        answer = keccak256(abi.encodePacked(word));
+        prize = msg.value;
+        emit NewAnswer();
+    }
+    
+    function guessAnswer(string memory word) public {
+        require(answer != 0, "Puzzle solve is paused.");
+        require(keccak256(abi.encodePacked(word)) == answer, "Wrong answer!");
+        
+        (bool success, ) = msg.sender.call{value: prize}("");
+        require(success);
+        
+        emit AnswerGuessed(msg.sender, prize);
+        prize = 0;
+        answer = 0;
+    }
+
+    function isPaused() public view returns (bool) {
+        return answer == 0;
+    }
+}
