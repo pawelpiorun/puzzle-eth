cc7192c7b8d3aaa4abfe9ea50822470a94e0c910
Paweł Piorun <dev.pawelpiorun@gmail.com>
2021-10-27 11:56:28 +0200

    Fix contract license, put ABI in JS file instead of env variables in order to make it work in production

--- src/puzzle.js
+++ src/puzzle.js
@@ -1,7 +1,9 @@
 import web3 from './web3'
 
+const abi = '[{"inputs":[],"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"winner","type":"address"},{"indexed":false,"internalType":"uint256","name":"prize","type":"uint256"}],"name":"AnswerGuessed","type":"event"},{"anonymous":false,"inputs":[],"name":"NewAnswer","type":"event"},{"inputs":[{"internalType":"string","name":"word","type":"string"}],"name":"guessAnswer","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"isPaused","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"prize","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"puzzleID","outputs":[{"internalType":"uint64","name":"","type":"uint64"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"string","name":"word","type":"string"},{"internalType":"uint64","name":"id","type":"uint64"}],"name":"setNewAnswer","outputs":[],"stateMutability":"payable","type":"function"}]';
+
 const puzzle = new web3.eth.Contract(
-    JSON.parse(process.env.REACT_APP_PUZZLE_ABI),
+    JSON.parse(abi),
     process.env.REACT_APP_PUZZLE_ADDRESS
 )
 
